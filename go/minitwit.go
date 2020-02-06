c25f187d53054e069292c519675d18a6c66c6a5e
mack <mack@itu.dk>
2020-02-06 21:32:55 +0100

    Finished golang skeleton

--- go/minitwit.go
+++ go/minitwit.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var DATABASE = connect_db()
+var SECRET_KEY = []byte("development key")
+var STORE = sessions.NewCookieStore(SECRET_KEY)
 
 func main() {
 
@@ -42,6 +47,31 @@ func connect_db() *sql.DB {
 
 func init_db() {
 	//Initialize the database tables
+	file, err := os.Open("schema.sql")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var currentLine string
+	statement := ""
+	for scanner.Scan() { //Read lines in schema.sql until semicolon which triggers the execute command.
+		currentLine = scanner.Text()
+		statement = statement + currentLine
+		if currentLine == ";" {
+			execCommand(statement)
+			statement = "" //Reset statement string
+		}
+	}
+}
+
+func execCommand(sqlCommand string) {
+	statement, err := DATABASE.Prepare(sqlCommand)
+	if err != nil {
+		fmt.Println(err)
+	}
+	statement.Exec()
 }
 
 func query_db(query string, args []string, one bool) {
@@ -90,15 +120,50 @@ func follow_user(w http.ResponseWriter, r *http.Request) {
 func unfollow_user(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "unfollow_user")
 }
-func logout(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "logout hit")
-}
+
 func add_message(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "add_message hit")
 }
+
 func login(w http.ResponseWriter, r *http.Request) {
+	session, err := STORE.Get(r, "session")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	fmt.Fprint(w, "login hit")
+	username := r.FormValue("username")
+	//password := r.FormValue("password")
+	//redirectTarget := "timeline"
+	user, err := DATABASE.Query("select * from user where username = ?", username)
+	if user.Err != nil { //Jeg skal tjekke om jeg har fundet en bruger. Idk man
+		fmt.Println("Invalid username")
+	}
+	//check password hash osv osv.
+
+	session.AddFlash("You were logged in")
+
 }
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "logout hit")
+	session, err := STORE.Get(r, "session")
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	session.AddFlash("You were logged out")
+	session.Values["user_id"] = nil
+	sessions.Save(r, w)
+	http.Redirect(w, r, "public_timeline", http.StatusFound)
+
+}
+
 func register(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "register hit")
 }
