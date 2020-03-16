d0a3647e7c795a94b07a0c9c75dfbc5e9b8037ca
fhoa <fhoa@itu.dk>
2020-03-16 08:38:45 +0100

    Ignore md5 security issue. Tell CI that no hardcoded password is used

--- app/src/utils/errormessages.go
+++ app/src/utils/errormessages.go
@@ -3,7 +3,7 @@ package utils
 const (
 	USERNAME_TAKEN                   = "The username is already taken"
 	PASSWORDS_DOES_NOT_MATCH_MESSAGE = "The two passwords do not match"
-	YOU_HAVE_TO_ENTER_A_PASSWORD     = "You have to enter a password"
+	YOU_HAVE_TO_ENTER_A_PASSWORD     = "You have to enter a password" /* #nosec G101 */
 	ENTER_A_VALID_EMAIL              = "You have to enter a valid email address"
 	ENTER_A_USERNAME                 = "You have to enter a username"
 )
