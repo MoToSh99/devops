7482af1e6b74a9767c3a94263e8e6c750641be57
fhoa <fhoa@itu.dk>
2020-03-17 17:17:47 +0100

    Fixed change request to remove comment

--- app/src/minitwit.go
+++ app/src/minitwit.go
@@ -13,7 +13,6 @@ import (
 
 func main() {
 	gob.Register(&types.User{})
-	//s := server.CreateNewServer("sqlite3", "/tmp/minitwit.db")
 	connectionString := "host=127.0.0.1 port=5432 user=" + os.Getenv("POSTGRES_USER") + " dbname=" + os.Getenv("POSTGRES_DB") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " sslmode=disable"
 	s := server.CreateNewServer("postgres", connectionString)
 	defer s.ShutDown()
