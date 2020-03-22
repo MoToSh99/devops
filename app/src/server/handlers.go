c37ddbbbd401832ef44e47e60cf73484ea70d88c
Mads Pedersen <madpe@itu.dk>
2020-03-22 12:47:49 +0100

    fixed issue #110

--- app/src/server/handlers.go
+++ app/src/server/handlers.go
@@ -421,8 +421,9 @@ func (s *Server) tweetsUsernamePost(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
 	var tweet types.TweetRequest
+
 	tweetErr := decoder.Decode(&tweet)
-	if tweetErr != nil {
+	if tweetErr != nil || tweet.Content == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	} else {
